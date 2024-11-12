1b8c6d17d19f360a664ea6e587e917119bf5380c
Lucas Vinicius <howaitosame13@gmail.com>
2024-11-12 08:37:35 -0300

    added favicon

--- sistema.php
+++ sistema.php
@@ -14,10 +14,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sidebar With Bootstrap</title>
+    <title>Vorg Technologies</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link rel="stylesheet" href="./styles/sistema.css">
+    <link rel="icon" href="./assets/imagens/logo-vorg/vorg-icon-black.png">
 </head>
 
 <body>
